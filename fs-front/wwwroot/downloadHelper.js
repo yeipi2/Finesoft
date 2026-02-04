b04c2eddb86b975f029325a504d4460e7a376621
yeipi2 <elyeipi23@gmail.com>
2026-02-04 09:06:24 -0700

    Problemas contizaciones solucionados

---
+++ fs-front/wwwroot/downloadHelper.js
@@ -0,0 +1,23 @@
+﻿window.downloadFile = function (fileName, base64Content) {
+    // Detectar el tipo MIME desde el contenido
+    let mimeType = 'application/octet-stream';
+
+    // Si es PDF
+    if (fileName.toLowerCase().endsWith('.pdf')) {
+        mimeType = 'application/pdf';
+    }
+    // Si es HTML
+    else if (fileName.toLowerCase().endsWith('.html')) {
+        mimeType = 'text/html';
+    }
+
+    // Crear el enlace de descarga
+    const link = document.createElement('a');
+    link.download = fileName;
+    link.href = `data:${mimeType};base64,${base64Content}`;
+
+    // Agregar al DOM, hacer clic y remover
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
